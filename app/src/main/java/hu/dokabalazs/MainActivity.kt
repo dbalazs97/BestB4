9035b241b3b2a01fdaa9f8a5e058e87d8fc34595
Dóka Balázs <dbalazs40@gmail.com>
2019-05-08 18:56:01 +0200

    Separate recycler view into a Fragment

--- app/src/main/java/hu/dokabalazs/MainActivity.kt
+++ app/src/main/java/hu/dokabalazs/MainActivity.kt
@@ -4,19 +4,23 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
 import hu.dokabalazs.adapter.FoodAdapter
 import hu.dokabalazs.db.FoodDatabase
 import hu.dokabalazs.db.typeconverter.BitmapTypeConverter
-import hu.dokabalazs.model.Food
+import hu.dokabalazs.fragment.FoodListFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
+import kotlinx.android.synthetic.main.fragment_food_list.*
+
 
 class MainActivity : AppCompatActivity() {
+
+	private var firstRun: Boolean = true
+	private lateinit var currentFragment: Fragment
 
 	companion object {
 		const val REQUEST_IMAGE_CAPTURE = 1
@@ -32,27 +36,7 @@ class MainActivity : AppCompatActivity() {
 
 		BitmapTypeConverter.context = this
 
-		food_item_list.apply {
-			setHasFixedSize(true)
-			layoutManager = LinearLayoutManager(this@MainActivity)
-			adapter = FoodAdapter(
-				listOf(
-					Food(1, "Milk", "10 litres", GregorianCalendar(2019, 5, 10).time, null),
-					Food(1, "Eggs", "15", GregorianCalendar(2019, 10, 15).time, null),
-					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
-					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
-					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
-					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
-					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
-					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
-					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
-					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
-					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
-					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
-					Food(1, "Spread Butter", "500 grammes", GregorianCalendar(2021, 2, 11).time, null)
-				)
-			)
-		}
+		changeFragment(FoodListFragment())
 
 		fab.setOnClickListener { takeThumbnail() }
 	}
@@ -67,6 +51,16 @@ class MainActivity : AppCompatActivity() {
 			R.id.action_settings -> true
 			else -> super.onOptionsItemSelected(item)
 		}
+	}
+
+	private fun changeFragment(fragment: Fragment) {
+		currentFragment = fragment
+		val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+		fragmentTransaction.apply {
+			addToBackStack(null)
+			replace(R.id.fragment_container, currentFragment)
+		}.commit()
 	}
 
 	private fun takeThumbnail() {
---
+++ app/src/main/java/hu/dokabalazs/fragment/FoodListFragment.kt
@@ -0,0 +1,50 @@
+package hu.dokabalazs.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import hu.dokabalazs.R
+import hu.dokabalazs.adapter.FoodAdapter
+import hu.dokabalazs.model.Food
+import kotlinx.android.synthetic.main.fragment_food_list.*
+import java.util.*
+
+
+class FoodListFragment : Fragment() {
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+		return inflater.inflate(R.layout.fragment_food_list, container, false)
+	}
+
+	override fun onActivityCreated(savedInstanceState: Bundle?) {
+		super.onActivityCreated(savedInstanceState)
+		initList()
+	}
+
+	private fun initList() {
+		food_item_list.apply {
+			setHasFixedSize(true)
+			layoutManager = LinearLayoutManager(requireActivity())
+			adapter = FoodAdapter(
+				listOf(
+					Food(1, "Milk", "10 litres", GregorianCalendar(2019, 5, 10).time, null),
+					Food(1, "Eggs", "15", GregorianCalendar(2019, 10, 15).time, null),
+					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
+					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
+					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
+					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
+					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
+					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
+					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
+					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
+					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
+					Food(1, "Salami", "750 grammes", GregorianCalendar(2020, 6, 27).time, null),
+					Food(1, "Spread Butter", "500 grammes", GregorianCalendar(2021, 2, 11).time, null)
+				)
+			)
+		}
+	}
+}
