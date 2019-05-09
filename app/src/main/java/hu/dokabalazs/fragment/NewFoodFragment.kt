ee54947ff1b28f20784dd5f47a29849ec7b232dc
balazs <dbalazs40@gmail.com>
2019-05-09 17:44:08 +0200

    Add validator for form

--- app/src/main/java/hu/dokabalazs/fragment/NewFoodFragment.kt
+++ app/src/main/java/hu/dokabalazs/fragment/NewFoodFragment.kt
@@ -1,12 +1,14 @@
 package hu.dokabalazs.fragment
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import hu.dokabalazs.R
 import kotlinx.android.synthetic.main.fragment_new_food.*
+import java.util.*
 
 class NewFoodFragment : Fragment() {
 
@@ -20,6 +22,35 @@ class NewFoodFragment : Fragment() {
 			it["barcode"]?.let { barcode ->
 				newfood_qr.setText(barcode.toString())
 			}
+		}
+
+		newfood_expiry_date.minDate = GregorianCalendar().timeInMillis
+		newfood_save_button.setOnClickListener { saveNewFood() }
+	}
+
+
+	private fun validateFields(): Boolean {
+		val name = newfood_name.text
+		val quantity = newfood_quantity.text
+		val expiryYear = newfood_expiry_date.year
+		val expiryMonth = newfood_expiry_date.month
+		val expiryDay = newfood_expiry_date.dayOfMonth
+
+		when {
+			name.length < 0 ->
+				view?.let { Snackbar.make(it, "The name can not be empty", 3000).show() }
+			quantity.length < 0 ->
+				view?.let { Snackbar.make(it, "The quantity can not be empty", 3000).show() }
+			GregorianCalendar(expiryYear, expiryMonth, expiryDay) < GregorianCalendar() ->
+				view?.let { Snackbar.make(it, "Can not put in expired food", 3000).show() }
+			else -> return true
+		}
+		return false
+	}
+
+	private fun saveNewFood() {
+		if (validateFields()) {
+			view?.let { Snackbar.make(it, "Values OK", 3000).show() }
 		}
 	}
 }
