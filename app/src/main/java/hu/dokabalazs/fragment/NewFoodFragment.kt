c90b1d69147ac0b62c7247adf46952533033cbb1
balazs <dbalazs40@gmail.com>
2019-05-09 15:21:18 +0200

    Fix new food layout

--- app/src/main/java/hu/dokabalazs/fragment/NewFoodFragment.kt
+++ app/src/main/java/hu/dokabalazs/fragment/NewFoodFragment.kt
@@ -6,10 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import hu.dokabalazs.R
+import kotlinx.android.synthetic.main.fragment_new_food.*
 
 class NewFoodFragment : Fragment() {
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		return inflater.inflate(R.layout.fragment_new_food, container, false)
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		arguments?.let {
+			it["barcode"]?.let { barcode ->
+				newfood_qr.setText(barcode.toString())
+			}
+		}
 	}
 }
