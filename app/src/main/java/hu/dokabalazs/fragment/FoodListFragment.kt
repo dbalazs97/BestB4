5bd54a45716167eee988d3d86b48928508767728
Dóka Balázs <dbalazs40@gmail.com>
2019-05-09 23:54:44 +0200

    Add swipe-to-delete

--- app/src/main/java/hu/dokabalazs/fragment/FoodListFragment.kt
+++ app/src/main/java/hu/dokabalazs/fragment/FoodListFragment.kt
@@ -3,10 +3,11 @@ package hu.dokabalazs.fragment
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import hu.dokabalazs.R
 import hu.dokabalazs.adapter.FoodAdapter
 import hu.dokabalazs.db.FoodDatabase
 import kotlinx.android.synthetic.main.fragment_food_list.*
@@ -17,7 +18,7 @@ class FoodListFragment : Fragment() {
 	lateinit var foodAdapter: FoodAdapter
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-		return inflater.inflate(R.layout.fragment_food_list, container, false)
+		return inflater.inflate(hu.dokabalazs.R.layout.fragment_food_list, container, false)
 	}
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -26,6 +27,20 @@ class FoodListFragment : Fragment() {
 	}
 
 	private fun initList() {
+
+		val itemTouchHelperCallback =
+			object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+				override fun onMove(
+					recyclerView: RecyclerView,
+					viewHolder: RecyclerView.ViewHolder,
+					target: RecyclerView.ViewHolder
+				): Boolean = false
+
+				override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+					foodAdapter.deleteAt(viewHolder.adapterPosition)
+				}
+			}
+
 		thread {
 			val foods = FoodDatabase.get().foodDao().getAllFoods()
 			foodAdapter = FoodAdapter(foods)
@@ -35,6 +50,7 @@ class FoodListFragment : Fragment() {
 					layoutManager = LinearLayoutManager(requireActivity())
 					adapter = foodAdapter
 				}
+				ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(food_item_list)
 			}
 		}
 	}
